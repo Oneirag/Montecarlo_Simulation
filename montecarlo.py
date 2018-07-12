f1ec0ca5df616ab424e84e9c78f84ffc52a3f897
Oscar Neira <Oneirag@yahoo.es>
2018-07-12 06:37:26 +0200

    finished test of statistical properties

--- montecarlo.py
+++ montecarlo.py
@@ -42,15 +42,20 @@ def simulate_gbm(S0, vols, corr, T=1, mu=0, n_steps=2, n_sim=100, show_plt=False
     W = np.cumsum(eps, axis=1) * np.sqrt(dt)  ### standard brownian motion ###
     W[:, 0, :] = 0  # To make it coherent with dt that starts in 0
     X = (mu - 0.5 * sig ** 2) * t + sig * W
+    # Wikipedia: step=np.exp((mu-sigma**2/2)*dt)*np.exp(sigma*np.random.normal(0,np.sqrt(dt),(1,n)))
+
     S = S0 * np.exp(X)  ### geometric brownian motion ###
     if show_plt:
         for i in range(S.shape[-1]):
             plt.plot(t[0, :, 0], np.swapaxes(S[:, :, i], 0, 1))
+            # plot mean on top
+            plt.plot(t[0, :, 0], S[:, :, i].mean(axis=0), linewidth=4, color='k')
+
         plt.show()
     return S
 
 
-def calculate_margin(S, freeze_dim=(-1,)):
+def calculate_margin(S, freeze_dim=-1):
     """
     simple margin calculator margin = power - fuel * production at delivery
     :param S: simulation matrix of 3 variables. First is power, second fuel third production
@@ -59,9 +64,18 @@ def calculate_margin(S, freeze_dim=(-1,)):
     :return:
     """
 
-    power, fuel, production = [S[:, -1 if i_var not in freeze_dim else 0, 0] for i_var in range(3)]
-    margin = (power - fuel) * production
+    if isinstance(freeze_dim, int):
+        freeze_dim_iter = (freeze_dim, )
+    else:
+        freeze_dim_iter = freeze_dim
 
+    vars = [S[:, -1 if i_var not in freeze_dim_iter else 0, 0] for i_var in range(S.shape[2])]
+    if len(vars) == 3:
+        power, fuel, production = vars
+        margin = (power - fuel) * production
+    else:
+        power, fuel = vars
+        margin = (power - fuel) * 100
     return margin
 
 
@@ -91,8 +105,19 @@ def check_statistical_properties(S, T):
     """
 
     # compute returns
-    logreturns = np.log(S[:, :-2, :] / S[:, 1:, :] - 1)
-    print(np.std(logreturns, axis=1) * np.sqrt(S.shape[1]) / np.sqrt(250 * T))
+    logreturns = np.log(S[:, :-1, :] / S[:, 1:, :])
+    anualized_vols = np.std(logreturns, axis=1).mean(axis=0) * np.sqrt(S.shape[1] / T)
+    print(f"Anualized vols: {anualized_vols}")
+    # correlation
+    corr = []
+    for i in range(S.shape[2]):
+        for j in range(i + 1, S.shape[2]):
+            corr_sim = 0
+            for i_sim in range(S.shape[0]):
+                corr_sim += np.corrcoef(logreturns[i_sim, :, i],
+                                       logreturns[i_sim, :, j], rowvar=False)[1, 0]
+            corr.append(corr_sim / S.shape[0])
+    print(f"Correlations: {corr}")
 
 
 def main():
@@ -116,13 +141,18 @@ def main():
                      [rho13, rho23, 1]])
 
     T = 1
+    #S = simulate_gbm(S0, vols, corr, T=T, mu=0, n_steps=50, n_sim=1000, show_plt=True)
     S = simulate_gbm(S0, vols, corr, T=T, mu=0, n_steps=50, n_sim=1000)
 
-    S0 = S0[:2]
-    vols = vols[:2]
-    corr = corr[:2, :2]
-
-    S = simulate_gbm(S0, vols, corr, T=T, mu=0, n_steps=50, n_sim=10, debug_calcs=True)
+    # S0 = S0[:2]
+    # vols = vols[:2]
+    # corr = corr[:2, :2]
+    #
+    # print(f"Input vols: {vols}")
+    # print(f"Input corr: {corr}")
+    #
+    # #S = simulate_gbm(S0, vols, corr, T=T, mu=0, n_steps=50, n_sim=10, debug_calcs=True)
+    # S = simulate_gbm(S0, vols, corr, T=T, mu=0, n_steps=50, n_sim=10)
 
     check_statistical_properties(S, T)
 
